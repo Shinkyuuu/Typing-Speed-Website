10e153263c8e4e0fcdc50ca4822ca9c8903adce3
Shinkyuuu <codyspark@gmail.com>
2021-08-14 15:14:18 -0700

    Added ';' to lines
    Feels like good practice man

--- scripts.js
+++ scripts.js
@@ -1,79 +1,83 @@
-const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random'
-const quoteDisplayElem = document.getElementById('quoteDisplay')
-const quoteInputElem = document.getElementById('quoteInput')
-const timerElem = document.getElementById('timer')
-let timerStart = false
-var timer
+const RANDOM_QUOTE_API_URL = 'https://api.quotable.io/random';
+const quoteDisplayElem = document.getElementById('quoteDisplay');
+const quoteInputElem = document.getElementById('quoteInput');
+const timerElem = document.getElementById('timer');
+let timerStart = false;
+var timer;
 
-document.addEventListener("keyup", function(event) {
-    if (event.code === 'Tab') {
-        renderNewQuote()
-    }
-})
+// document.addEventListener("keyup", function(event) {
+//     if (event.code === 'Tab') {
+//         renderNewQuote()
+//     }
+// })
+
+function resetOnTab() {
+    var key = window.event.keyCode;
+
+}
 
 quoteInputElem.addEventListener('input', () => {
-    const arrayQuote = quoteDisplayElem.querySelectorAll('span')
-    const arrayValue = quoteInputElem.value.split('')
-    let correct = true
+    const arrayQuote = quoteDisplayElem.querySelectorAll('span');
+    const arrayValue = quoteInputElem.value.split('');
+    let correct = true;
     
     if (timerStart == false && arrayValue != null) {
-        timerStart = true
-        setTimer()
+        timerStart = true;
+        setTimer();
     }
 
     arrayQuote.forEach((characterSpan, index) => {
-        const character = arrayValue[index]
+        const character = arrayValue[index];
         if (character == characterSpan.innerText) { // Correct  
-            characterSpan.classList.add('correct')
-            characterSpan.classList.remove('incorrect')
+            characterSpan.classList.add('correct');
+            characterSpan.classList.remove('incorrect');
         } else if (character == null) { // Empty  
-            characterSpan.classList.remove('incorrect')
-            characterSpan.classList.remove('correct')
-
-            correct = false
+            characterSpan.classList.remove('incorrect');
+            characterSpan.classList.remove('correct');
+            correct = false;
         } else { // Incorrect  
-            characterSpan.classList.add('incorrect')
-            characterSpan.classList.remove('correct')
-            correct = false
+            characterSpan.classList.add('incorrect');
+            characterSpan.classList.remove('correct');
+            correct = false;
         }
     })
 
-    if (correct == true) renderNewQuote()
+    if (correct == true) renderNewQuote();
 })
 
 function getRandomQuote() {
     return fetch(RANDOM_QUOTE_API_URL)
         .then(response => response.json())
-        .then(data => data.content)
+        .then(data => data.content);
 }
 
 async function renderNewQuote() {
-    const quote = await getRandomQuote()
-    quoteDisplayElem.innerText = ''
-    quoteInputElem.value = null
-    timerElem.innerText = 0
-    clearInterval(timer)
-    timerStart = false
+    const quote = await getRandomQuote();
+    quoteDisplayElem.innerText = '';
+    quoteInputElem.value = null;
+    timerElem.innerText = 0;
+    clearInterval(timer);
+    timerStart = false;
 
     quote.split('').forEach(character => {
-        const characterSpan = document.createElement('span')
-        characterSpan.innerText = character
-        quoteDisplayElem.appendChild(characterSpan)
+        const characterSpan = document.createElement('span');
+        characterSpan.innerText = character;
+        quoteDisplayElem.appendChild(characterSpan);
     })
 
-    console.log(quote)
+    console.log(quote);
 }
 
-let startTime
+let startTime;
 function setTimer() {
-    startTime = new Date()
-    timer = setInterval(startTimer, 1000)
+    startTime = new Date();
+    timer = setInterval(startTimer, 1000);
 }
 
 function startTimer() {
-    currentTime = Math.floor((new Date() - startTime) / 1000)
-    timerElem.innerText = currentTime
-    console.log(currentTime)
+    currentTime = Math.floor((new Date() - startTime) / 1000);
+    timerElem.innerText = currentTime;
+    console.log(currentTime);
 }
 
-renderNewQuote()
+renderNewQuote();
