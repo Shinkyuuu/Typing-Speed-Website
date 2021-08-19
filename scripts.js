dc29942b22801e02d85ec49010b3c667cfa1335c
Shinkyuuu <codyspark@gmail.com>
2021-08-18 20:20:32 -0700

    Split the quote into words rather than characters
    Quote is split into words which are split into characters. This is so we can calculate wpm (words per minute) and not cpm (characters per minute)

--- scripts.js
+++ scripts.js
@@ -52,11 +52,8 @@ quoteInputElem.addEventListener('input', () => {
         index += 1;
     })
 
-    quoteDisplayElem.insertBefore(
-        caretElem, 
-        quoteDisplayElem.querySelector("#a"+ arrayValue.length));
-
     if (correct == true) renderNewQuote();
+    document.getElementById('a'+arrayValue.length).parentNode.insertBefore(caretElem, quoteDisplayElem.querySelector("#a"+ arrayValue.length));
 })
 
 function getRandomQuote() {
@@ -65,23 +62,69 @@ function getRandomQuote() {
         .then(data => data.content);
 }
 
+
+// Create a new quote and split it into divs/spans
 async function renderNewQuote() {
     const quote = await getRandomQuote();
 
+    //setup the quote display and text input
     quoteDisplayElem.innerText = '';
     quoteInputElem.value = null;
+    //setup timer
     timerElem.innerText = 0;
     clearInterval(timer);
     timerStart = false;
 
-    quote.split('').forEach((character, index) => {
-        const characterSpan = document.createElement('span');
-        characterSpan.id = 'a' + index;
-        characterSpan.innerText = character;
-        quoteDisplayElem.appendChild(characterSpan);
+    // quote.split('').forEach((character, index) => {
+    //     const characterSpan = document.createElement('span');
+    //     characterSpan.id = 'a' + index;
+    //     characterSpan.innerText = character;
+    //     quoteDisplayElem.appendChild(characterSpan);
+    // })
 
-    })
-    quoteDisplayElem.insertBefore(caretElem, quoteDisplay.firstElementChild);
+    // quote.split(' ').forEach((word) => {
+    //     const wordDiv = document.createElement('div');
+    //     wordDiv.classList.add("word");
+    //     word.split('').forEach((character, index) => {
+    //         const characterSpan = document.createElement('span');
+    //         characterSpan.id = 'a'+index;
+    //         characterSpan.innerText = character;
+    //         wordDiv.appendChild(characterSpan);
+    //     })
+
+    //     quoteDisplayElem.appendChild(wordDiv);
+    //     const space = document.createElement('span');
+    //     space.innerText = ' ';
+    //     quoteDisplayElem.appendChild(space);
+    // })
+
+    //split the quote
+    var quoteList = quote.split(' ');
+    var characterCount = 0;
+
+    for (let i = 0; i < quoteList.length; i++) {
+        const wordDiv = document.createElement('div');
+        wordDiv.classList.add("word");
+        quoteList[i].split('').forEach((character) => {
+            const characterSpan = document.createElement('span');
+            characterSpan.id = 'a'+characterCount;
+            characterSpan.innerText = character;
+            wordDiv.appendChild(characterSpan);
+
+            characterCount++;
+        });
+
+        quoteDisplayElem.appendChild(wordDiv);
+        const characterSpace = document.createElement('span');
+        characterSpace.id = 'a'+characterCount;
+        characterSpace.innerText = ' ';
+        quoteDisplayElem.appendChild(characterSpace);
+        characterCount += 1;
+    }
+
+    quoteDisplayElem.removeChild(quoteDisplayElem.lastChild);
+    characterCount -= 1;
+    quoteDisplayElem.firstElementChild.insertBefore(caretElem, document.getElementById("a0"));
 
     console.log(quote);
 }
@@ -95,7 +138,7 @@ function setTimer() {
 function startTimer() {
     currentTime = Math.floor((new Date() - startTime) / 1000);
     timerElem.innerText = currentTime;
-    console.log(currentTime);
+    //console.log(currentTime);
 }
 
 function ready() {
